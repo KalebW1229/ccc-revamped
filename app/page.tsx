a7358e9d74c68d761477a241e17c34e75e416a5f
Kaleb <kalebcward@gmail.com>
2025-04-10 16:45:38 -0400

    built first, now trying images

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src='/next.svg'
+          src='/ccc-revamped/next.svg'
           alt="Next.js logo"
           width={180}
           height={38}
@@ -34,7 +34,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src='/dist/vercel.svg'
+              src='/ccc-revamped/vercel.svg'
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -60,7 +60,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/file.svg`}
+            src='/ccc-revamped/file.svg'
             alt="File icon"
             width={16}
             height={16}
@@ -75,7 +75,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/window.svg`}
+            src='/ccc-revamped/window.svg'
             alt="Window icon"
             width={16}
             height={16}
@@ -90,7 +90,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/globe.svg`}
+            src='/ccc-revamped/globe.svg'
             alt="Globe icon"
             width={16}
             height={16}
